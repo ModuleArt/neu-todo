47e6b5a65a014bd255afc268abfc019b94266987
Eugene <40366303+Beelink@users.noreply.github.com>
2022-03-11 01:42:44 +0200

    Use fuzzy-search to search for tasks

--- src/pages/SearchPage/SearchPage.vue
+++ src/pages/SearchPage/SearchPage.vue
@@ -11,7 +11,7 @@
             hide-details
             solo
             flat
-            placeholder="Search for tasks titles, descriptions and steps"
+            placeholder="Search for tasks titles, descriptions, etc."
             prepend-inner-icon="mdi-magnify"
             color="primary"
             outlined
@@ -19,7 +19,25 @@
             v-model="searchText"
             class="search-page__search-field"
             ref="searchInput"
+            @input="search"
           />
+          <v-card
+            outlined
+            class="search-page__card mt-2"
+            v-for="todo in todos"
+            :key="todo.id"
+          >
+            <v-card-title class="py-2">{{ todo.title }}</v-card-title>
+            <v-divider v-if="todo.body" />
+            <v-card-text v-if="todo.body" class="py-2">
+              {{ todo.body }}
+            </v-card-text>
+          </v-card>
+          <div v-if="!todos.length">
+            <v-subheader class="pa-0">
+              Nothing to show.
+            </v-subheader>
+          </div>
         </v-col>
       </v-row>
     </v-container>
@@ -29,6 +47,13 @@
 <script lang="ts">
 // utils
 import { Vue, Component } from "@/utils/vue-imports";
+import FuzzySearch from "fuzzy-search";
+
+// store modules
+import { todosModule } from "@/store";
+
+// interfaces
+import Todo from "@/interfaces/entities/todo";
 
 // component
 @Component({
@@ -42,6 +67,10 @@ export default class SearchPage extends Vue {
 
   // data
   private searchText = "";
+  private searcher = new FuzzySearch(todosModule.todos, ["title", "body"], {
+    caseSensitive: false,
+  });
+  private todos: Todo[] = [];
 
   // lifecycle
   private mounted() {
@@ -51,6 +80,15 @@ export default class SearchPage extends Vue {
   // private methods
   private goBack() {
     this.$router.push({ name: "main" });
+  }
+
+  private search(input: string) {
+    if (input.length) {
+      const results = this.searcher.search(input);
+      this.todos = results;
+    } else {
+      this.todos = [];
+    }
   }
 }
 </script>
